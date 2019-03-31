404fedea9bfdeaa3038598195bad94a153369ade
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2019-03-31 13:35:30 +0100

    Add handle request with payload
    StringDecoder, added dif parts of
    url, query, headers, method

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 const server = http.createServer((req, res) => {
+
   let parsedUrl = url.parse(req.url, true)
   let pathname = parsedUrl.pathname
   let trimedPath = pathname.replace(/^\/+|\/+$/g, '')
   let queryString = parsedUrl.query
+
+  let headers = req.headers
+  let method = req.method
+
+  console.log("Request made headers: \n" + JSON.stringify(headers))
+  console.log(`Request made method ${method}`)
+
+  let decoder = new StringDecoder('utf-8')
+  let buffer = ''
+
   console.log(`Request made path ${trimedPath}`)
   console.log(`Request made query is: ${JSON.stringify(queryString)}`)
-  res.end('Hello world');
+
+  req.on('data', (data) => {
+    buffer += decoder.write(data);
+  })
+
+  req.on('end', () => {
+    buffer += decoder.end()
+    res.end('Hello world');
+    console.log("Request made with payload", buffer)
+  })
+
 })
 
 server.listen(3000);
