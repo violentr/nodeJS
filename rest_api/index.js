5fa74e42c801791608be9642fda18e146d7c793d
Deniss B <den.b@itservicesgroup.co.uk>
2025-12-20 20:33:25 +0000

    Add improvement, necessary comments

--- rest_api/index.js
+++ rest_api/index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const url = require('url');
 const StringDecoder = require('string_decoder').StringDecoder;
+const PORT = 3000;
 
 const server = http.createServer((req, res) => {
 
@@ -10,25 +11,28 @@ const server = http.createServer((req, res) => {
   let queryString = parsedUrl.query
 
   let headers = req.headers
-  let method = req.method
+  let method = req.method.toLowerCase()
 
-  console.log("Request made headers: \n" + JSON.stringify(headers))
-  console.log(`Request made method ${method}`)
+  console.log("Request  headers: \n" + JSON.stringify(headers))
+  console.log(`Request  method ${method}`)
 
   let decoder = new StringDecoder('utf-8')
   let buffer = ''
 
-  console.log(`Request made path ${trimedPath}`)
-  console.log(`Request made query is: ${JSON.stringify(queryString)}`)
+  console.log(`Request  path ${trimedPath}`)
+  console.log(`Request  query is: ${JSON.stringify(queryString)}`)
 
+  // This is only needed for pure Node.js, req.body is available in express Js
+  // This will happen first and then the end event will be triggered
   req.on('data', (data) => {
     buffer += decoder.write(data);
   })
-
+  // This part of the code will be executed after the data event is triggered !
   req.on('end', () => {
     buffer += decoder.end()
-    let routerPath = router[trimedPath]
-    let chosenHandler = typeof(router) !== 'undefined' ? routerPath : handlers.notFound
+    const path = router[trimedPath]
+    let chosenHandler = typeof(path) !== 'undefined' ? path : handlers.notFound
+    
     let data = {
       trimedPath,
       queryString,
@@ -36,13 +40,20 @@ const server = http.createServer((req, res) => {
       headers,
       buffer
     }
+    // This part of the code will be executed after the end event is triggered !
     chosenHandler(data, (statusCode, payload) =>{
       statusCode = typeof(statusCode) === 'number' ? statusCode : 200
+      
       payload = typeof(payload) === 'object' ? payload : {}
+      
       payloadString = JSON.stringify(payload)
+      // This part of the code will be executed after the chosenHandler is triggered !
       res.setHeader('Content-Type', 'application/json');
+      
       res.writeHead(statusCode)
+      
       res.end(payloadString);
+      console.log("Body data is: ", data.buffer);
       console.log("Response is: ", statusCode, payloadString)
     })
 
@@ -50,9 +61,10 @@ const server = http.createServer((req, res) => {
 
 })
 
-server.listen(3000);
-
-//Request router
+// Running server
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`)
+});
 
 let handlers = {};
 
@@ -60,10 +72,12 @@ let handlers = {};
 handlers.sample = (data, callback) =>{
   callback(406, {'name': 'sample handler'})
 }
+// Not found handler
 handlers.notFound = (data, callback) =>{
-  callback(404)
+  callback(404, {'error': 'Not Found'})
 }
 
+// Router with path and handler
 let router = {
   'sample': handlers.sample
 }
