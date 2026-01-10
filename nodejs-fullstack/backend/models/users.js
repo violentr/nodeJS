332f3042ba7a02c001c0b0abf5a62d77321e929a
Deniss B <den.b@itservicesgroup.co.uk>
2026-01-10 15:19:03 +0000

    Add validation for User model

--- nodejs-fullstack/backend/models/users.js
+++ nodejs-fullstack/backend/models/users.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const Joi = require('joi');
 const userSchema = new mongoose.Schema({
     firstname: {type: String, default: null},
     lastName:{type: String, default: null},
@@ -9,4 +9,16 @@ const userSchema = new mongoose.Schema({
     token: {type: String},
 });
 
-module.exports = mongoose.model("user", userSchema);
+const User = mongoose.model("user", userSchema);
+const validate = (user) => {
+    const schema = Joi.object({
+        firstName: Joi.string().required,
+        lastName: Joi.string().required,
+        userName: Joi.string().required,
+        email: Joi.string().required,
+        password: Joi.string().required,
+    })
+    return schema.validate(user);
+}
+
+module.exports = { User, validate }
