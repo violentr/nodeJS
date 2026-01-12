661b8384b1a29c40950e401d09cf830501f2cd84
Deniss B <den.b@itservicesgroup.co.uk>
2026-01-12 19:55:39 +0000

    Fix model attributes and typos

--- nodejs-fullstack/backend/models/user.js
+++ nodejs-fullstack/backend/models/user.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
 const userSchema = new mongoose.Schema({
-    firstname: {type: String, default: null},
+    firstName: {type: String, default: null},
     lastName:{type: String, default: null},
-    userName:{type: String, unique: true},
+    username:{type: String, unique: true},
     email: {type: String, unique: true},
     password: {type: String},
     token: {type: String},
@@ -12,11 +12,11 @@ const userSchema = new mongoose.Schema({
 const User = mongoose.model("user", userSchema);
 const validate = (user) => {
     const schema = Joi.object({
-        firstName: Joi.string().required,
-        lastName: Joi.string().required,
-        userName: Joi.string().required,
-        email: Joi.string().required,
-        password: Joi.string().required,
+        firstName: Joi.string().required(),
+        lastName: Joi.string().required(),
+        username: Joi.string().required(),
+        email: Joi.string().required(),
+        password: Joi.string().required(),
     })
     return schema.validate(user);
 }
