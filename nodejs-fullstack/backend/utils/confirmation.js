f8d8041ee12e93933f797f507ebedb3a46c47b7a
Deniss B <den.b@itservicesgroup.co.uk>
2026-01-14 17:43:25 +0000

    Add improve encrypt/decrypt functionality

--- nodejs-fullstack/backend/utils/confirmation.js
+++ nodejs-fullstack/backend/utils/confirmation.js
@@ -1,18 +1,30 @@
 const crypto = require('crypto');
-require('dotenv').config();
 const algorithm = process.env.CRYPTO_ALGORITHM;
 const secretKey = process.env.CONFIRMATION_SECRET_KEY;
-const iv = process.env.INITIALIZATION_VECTOR;
+const iv = crypto.randomBytes(16);
 
+console.log("IV: ", iv);
 const encrypt = (token) => {
-    const cipher = crypto.createCipheriv(algorithm, secretKey, Buffer.from(iv, "hex"));
-    const encrypted = Buffer.concat([cipher.update(token), cipher.final()]);
-    return encrypted.toString();
+    const cipher = crypto.createCipheriv(
+        algorithm,
+        secretKey,
+        Buffer.from(iv, "hex")
+    );
+  const encrypted = Buffer.concat([cipher.update(token), cipher.final()]);
+
+  return encrypted.toString("hex");
 }
 
-const decrypt = (token) => {
-    const decipher = crypto.createDecipheriv(algorithm, secretKey, Buffer.from(iv, "hex"));
-    const decrypted = Buffer.concat([decipher.update(token), decipher.final()]);
+const decrypt = (hash) => {
+    const decipher = crypto.createDecipheriv(
+        algorithm,
+        secretKey,
+        Buffer.from(iv, "hex")
+    );
+    const decrypted = Buffer.concat([
+    decipher.update(Buffer.from(hash, "hex")),
+    decipher.final(),
+  ]);
     return decrypted.toString();
 }
 
