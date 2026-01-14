8f5ef5b2e67e402a7d404103f65747b433869cfb
Deniss B <den.b@itservicesgroup.co.uk>
2026-01-14 17:47:30 +0000

    Add user attr isConfirmed, verifiacation by email

--- nodejs-fullstack/backend/models/user.js
+++ nodejs-fullstack/backend/models/user.js
@@ -7,6 +7,7 @@ const userSchema = new mongoose.Schema({
     email: {type: String, unique: true},
     password: {type: String},
     token: {type: String},
+    isConfirmed: {type: Boolean, default: false}
 });
 
 const User = mongoose.model("user", userSchema);
