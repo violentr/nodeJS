6585475f0e97549ec5fae86380652d2c7b9c3d80
Deniss B <den.b@itservicesgroup.co.uk>
2026-01-14 17:48:24 +0000

    Add router for the email verification

--- nodejs-fullstack/routes/index.js
+++ nodejs-fullstack/routes/index.js
@@ -2,4 +2,5 @@ const router = require('express').Router();
 const userController = require('../backend/controllers/user');
 
 router.post("/signup", userController.signup);
+router.get('/verify/:confirmationToken', userController.verifyEmail);
 module.exports = router;
