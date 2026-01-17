32fa5a3292d09ac3bf46c5553eae278f088c1ade
Deniss B <den.b@itservicesgroup.co.uk>
2026-01-17 20:39:59 +0000

    Add Fix typo, make sure verify used

--- nodejs-fullstack/backend/middleware/auth.js
+++ nodejs-fullstack/backend/middleware/auth.js
@@ -7,7 +7,7 @@ middleware.verify = (req, res, next) => {
         return res.status(403).send("A token required for authentication");
     }
     try {
-        const decoded = jwt.sign(token, process.env.TOKEN_SECRET_KEY);
+        const decoded = jwt.verify(token, process.env.TOKEN_SECRET_KEY);
         req.user = decoded;
     }catch(error){
         return res.status(401).send("Invalid token");
