cef4ce70b801fc640a03f83ef4896de90a8a7782
Deniss B <den.b@itservicesgroup.co.uk>
2026-01-24 18:57:29 +0000

    Add Numbers calculations logic with UI and clear button

--- mern-training/app.js
+++ mern-training/app.js
@@ -8,6 +8,17 @@ const removeNumber = (myNumbers, numberToRemove) => {
   return myNumbers.filter((e) => e !== numberToRemove);
 }
 
+const sum = (numbers) =>{
+  return  numbers.reduce((sum, number) => sum += number,0);
+}
+const average = (numbers) =>{
+  return (sum(numbers)/numbers.length).toFixed(2);
+}
+
+const max = (numbers) => {
+  return Math.max(...numbers);
+}
+
 let savedData = localStorage.getItem("numbers");
 let numbers = savedData ? JSON.parse(savedData) : [];
 
@@ -15,9 +26,13 @@ const numberInput = document.getElementById('numberInput');
 const addBtn = document.getElementById('addBtn');
 const display = document.getElementById('display');
 const removeBtn = document.getElementById("removeBtn");
+const clearButton = document.getElementById("clearBtn");
 
 const render = () => {
   display.innerText = numbers.join(', ');
+  document.getElementById("sumDisplay").innerText = numbers.length > 0 ? sum(numbers) : 0
+  document.getElementById("avgDisplay").innerText = numbers.length > 0 ? average(numbers) : "N/A"
+  document.getElementById("maxDisplay").innerText = numbers.length > 0 ? max(numbers) : "N/A"
   localStorage.setItem("numbers", JSON.stringify(numbers));
 };
 
@@ -47,3 +62,8 @@ removeBtn.addEventListener('click', () => {
     numberInput.value = '';
   }
 });
+
+clearButton.addEventListener('click', () =>{
+  numbers = [];
+  render();
+})
