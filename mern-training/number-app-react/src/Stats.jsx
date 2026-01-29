9d8ceaace57c8ff1931239a31120c7c35009417f
Deniss B <den.b@itservicesgroup.co.uk>
2026-01-29 18:55:49 +0000

    Fix a bug with reduce funciton, crashes app when []

--- mern-training/number-app-react/src/Stats.jsx
+++ mern-training/number-app-react/src/Stats.jsx
@@ -1,10 +1,10 @@
 function Stats ({numbers}){
-  const sum = numbers.reduce((acc, n) => acc += n)
+  const sum = numbers.reduce((acc, n) => acc += n, 0)
   const average = numbers.length > 0 ? (sum/numbers.length).toFixed(2) : 'N/A';
   const max = numbers.length > 0 ? Math.max(...numbers) : 'N/A';
   return (
     <div className="stats">
-      <p>Sum: {numbers}</p>
+      <p>Sum: {sum}</p>
       <p>Average: {average}</p>
       <p>Max: {max}</p>
     </div>
