5eeedc73c246909f1ef72b958e1237b7c0cf0f39
Deniss B <den.b@itservicesgroup.co.uk>
2026-02-06 18:48:58 +0000

    Add persistance with mongoDb

--- mern-training/number-app-backend/server.js
+++ mern-training/number-app-backend/server.js
@@ -1,30 +1,46 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 let numbers = [10, 20, 30, 40];
 
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch(err => console.error("Could not connect to MongoDB", err));
+
+// 2. Define a Schema and Model
+const numberSchema = new mongoose.Schema({
+  value: Number,
+  
+  createdAt: { type: Date, default: Date.now }
+});
+
+const NumberModel = mongoose.model('Number', numberSchema);
+
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/numbers', (req,res)=>{
+app.get('/api/numbers', async (req,res)=>{
+    const docs = await NumberModel.find();
+    const numbers = docs.map(doc => doc.value);
     res.json(numbers);
 })
 
-app.post('/api/numbers', (req, res)=>{
+app.post('/api/numbers', async (req, res)=>{
     try {
         console.log(req.body);
         const {number} = req.body;
         if (typeof number === 'number'){
-            numbers.push(number)
-            res.status(200).json({message: "Number added !", numbers});
+            const newNumber = new NumberModel({ value: number });
+            await newNumber.save();
+            res.status(201).json({message: "Saved to DB!"});
         }else{
             res.status(400).json({message: "Invalid number !"})
         }
     }catch(err){
-        console.error(err);
-        process.exit(1)
+        res.status(400).json({ error: err.message });
     }
   
 });
