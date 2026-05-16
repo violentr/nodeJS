d9a1af7e84f02cd42ac24814696de56d6d6697b4
Deniss B <den.b@itservicesgroup.co.uk>
2026-05-16 12:50:50 +0100

    Fix express route, catch all case, specific Express 5

--- movie_reviews/backend/server.js
+++ movie_reviews/backend/server.js
@@ -7,6 +7,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/movies", movies);
-app.use("/{*path}", (req, res) => res.status(404).json({ error: "Route not found" }));
+app.use((req, res) => res.status(404).json({ error: "Route not found" }));
 
 export default app;
