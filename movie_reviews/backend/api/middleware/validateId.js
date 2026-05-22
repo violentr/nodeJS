c22e073fbe4ed43001c7059b97273a565b7ecd48
Deniss B <den.b@itservicesgroup.co.uk>
2026-05-22 16:03:55 +0100

    Add validateReviewId middleware

--- movie_reviews/backend/api/middleware/validateId.js
+++ movie_reviews/backend/api/middleware/validateId.js
@@ -1,15 +1,30 @@
 import mongodb from 'mongodb';
 
 const ObjectId = mongodb.ObjectId;
+const error_message = {error: "Server error"};
 
 export const validateMovieId = (req, res, next) => {
     try {
         const {id} = req.params;
+        error_message.error = "Movie not found";
         if (!ObjectId.isValid(id)) {
-            return res.status(404).json({error: 'Movie not found'});
+            return res.status(404).json(error_message);
         }
     } catch (err) {
-        return res.status(500).json({err: "Server error"});
+        return res.status(500).json(error_message);
+    }
+    next();
+};
+
+export const validateReviewId = (req, res, next) => {
+    const {review_id} = req.body;
+    try{
+        error_message.error = "Review not found";
+        if (!ObjectId.isValid(review_id)) {
+            return res.status(404).json(error_message);
+        }
+    }catch(err){
+        return res.status(500).json(error_message);
     }
     next();
 };
--- movie_reviews/backend/api/movies.route.js
+++ movie_reviews/backend/api/movies.route.js
@@ -3,6 +3,7 @@ import MoviesController from '../api/movies.controller.js';
 import ReviewsController from '../api/reviews.controller.js';
 // Middleware
 import { validateMovieId } from './middleware/validateId.js';
+import { validateReviewId } from './middleware/validateId.js';
 
 const router = express.Router();
 router.route('/').get(MoviesController.apiGetAllMovies);
@@ -10,7 +11,7 @@ router.route('/id/:id').get(validateMovieId, MoviesController.apiGetMovieById);
 
 router.route('/review')
     .post(ReviewsController.apiPostReview)
-    .put(ReviewsController.apiUpdateReview)
-    .delete(ReviewsController.apiDeleteReview)
+    .put(validateReviewId, ReviewsController.apiUpdateReview)
+    .delete(validateReviewId, ReviewsController.apiDeleteReview)
 
 export default router;
