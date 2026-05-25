420943fd80ce2920125f6e1c29c5a440b0a3b5bc
Deniss B <den.b@itservicesgroup.co.uk>
2026-05-25 12:04:15 +0100

    Add Simple NavBar with bootstrap

--- movie_reviews/frontend/src/App.js
+++ movie_reviews/frontend/src/App.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import {Switch, Route, Link} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import AddReview from './components/add-review'
+import MoviesList from './components/movies-list'
+import Movie from './components/movie'
+import Login from './components/login'
+
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Movie Reviews</h1>
-        <Link to="/movies">Movies</Link>
+        <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="/">Movies</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="#home" >Home</Nav.Link>
+                    <Nav.Link href="#link" >Link</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     </div>
   );
 }
