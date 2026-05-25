53eb62b56c6b32476e8ac488d7692abd1195e2b5
Deniss B <den.b@itservicesgroup.co.uk>
2026-05-25 11:45:12 +0100

    Add simple routing

--- movie_reviews/frontend/src/App.js
+++ movie_reviews/frontend/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Switch, Route, Link} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Movie Reviews</h1>
+        <Link to="/movies">Movies</Link>
     </div>
   );
 }
-
 export default App;
