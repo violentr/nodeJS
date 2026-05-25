32571350fdb897b48a1b3bd79b1a3fe5c9a32c58
Deniss B <den.b@itservicesgroup.co.uk>
2026-05-25 13:58:55 +0100

    Add simple login state with relevant links

--- movie_reviews/frontend/src/App.js
+++ movie_reviews/frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {useState} from 'react'
 import {Switch, Route, Link} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import AddReview from './components/add-review'
@@ -12,15 +13,28 @@ import Navbar from 'react-bootstrap/Navbar'
 
 
 function App() {
+    const [user, setUser] = useState(null);
+
+    async function login(user=null){
+        setUser(user);
+    }
+    async function logout(){
+        setUser(null);
+    }
+
   return (
     <div className="App">
         <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="/">Movies</Navbar.Brand>
+            <Navbar.Brand href="/">Movie Reviews</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav"/>
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="#home" >Home</Nav.Link>
-                    <Nav.Link href="#link" >Link</Nav.Link>
+                    <Nav.Link>
+                        <Link to={"/movies"}>Movies</Link>
+                    </Nav.Link>
+                    <Nav.Link>
+                        {user ? (<a onClick={logout}>Logout</a>) : (<Link to={"/login"}>Login</Link>)}
+                    </Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
