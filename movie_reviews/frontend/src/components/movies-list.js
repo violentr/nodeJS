6b5d5afb33759620135a253c28d0d709b119332e
Deniss B <den.b@itservicesgroup.co.uk>
2026-06-09 18:02:35 +0100

    Add Card Markup with Movie information

--- movie_reviews/frontend/src/components/movies-list.js
+++ movie_reviews/frontend/src/components/movies-list.js
@@ -6,6 +6,7 @@ import Button from "react-bootstrap/Button"
 import Col from "react-bootstrap/Col"
 import Row from "react-bootstrap/Row"
 import Container from "react-bootstrap/Container"
+import Card from "react-bootstrap/Card"
 
 const MoviesList = (props) => {
   const [movies, setMovies] = useState([])
@@ -72,39 +73,58 @@ const MoviesList = (props) => {
 
   return (
     <div className="app">
-      <Form>
+      <Container>
+        <Form>
+          <Row>
+            <Col>
+              <Form.Group>
+                <Form.Control
+                  type="text"
+                  placeholder="Search by title"
+                  value={searchTitle}
+                  onChange={onChangeSearchTitle}
+                />
+              </Form.Group>
+              <Button variant="primary" type="button" onClick={findByTitle}>
+                Search
+              </Button>
+            </Col>
+            <Col>
+              <Form.Group>
+                <Form.Control as="select" onChange={onChangeSearchRating}>
+                  {ratings.map((rating) => {
+                    return (
+                      <option key={rating} value={rating}>
+                        {rating}
+                      </option>
+                    )
+                  })}
+                </Form.Control>
+              </Form.Group>
+              <Button variant="primary" type="button" onClick={findByRating}>
+                Search
+              </Button>
+            </Col>
+          </Row>
+        </Form>
         <Row>
-          <Col>
-            <Form.Group>
-              <Form.Control
-                type="text"
-                placeholder="Search by title"
-                value={searchTitle}
-                onChange={onChangeSearchTitle}
-              />
-            </Form.Group>
-            <Button variant="primary" type="button" onClick={findByTitle}>
-              Search
-            </Button>
-          </Col>
-          <Col>
-            <Form.Group>
-              <Form.Control as="select" onChange={onChangeSearchRating}>
-                {ratings.map((rating) => {
-                  return (
-                    <option key={rating} value={rating}>
-                      {rating}
-                    </option>
-                  )
-                })}
-              </Form.Control>
-            </Form.Group>
-            <Button variant="primary" type="button" onClick={findByRating}>
-              Search
-            </Button>
-          </Col>
+          {movies.map((movie) => {
+            return (
+              <Col>
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img src={movie.poster + "/100px180"} />
+                  <Card.Body>
+                    <Card.Title>{movie.title}</Card.Title>
+                    <Card.Text>Rating: {movie.rated}</Card.Text>
+                    <Card.Text>{movie.plot}</Card.Text>
+                    <Link to={"/movies/" + movie._id}> View Reviews</Link>
+                  </Card.Body>
+                </Card>
+              </Col>
+            )
+          })}
         </Row>
-      </Form>
+      </Container>
     </div>
   )
 }
