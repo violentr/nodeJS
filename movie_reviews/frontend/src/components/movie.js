979f494177cfe911057766a997fb53f3ef43b57b
Deniss B <den.b@itservicesgroup.co.uk>
2026-06-29 18:08:32 +0100

    Update Movie component, /movies/:movie_id

--- movie_reviews/frontend/src/components/movie.js
+++ movie_reviews/frontend/src/components/movie.js
@@ -1,6 +1,27 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import MovieDataService from "../services/movies"
+import { Link, useParams } from "react-router-dom"
 
-function Movie() {
-  return <div className="App">Movie</div>
+const Movie = () => {
+  const { id } = useParams()
+  const [movie, setMovie] = useState({
+    id: null,
+    title: "",
+    rated: "",
+    reviews: [],
+  })
+  const getMovie = (movieId) => {
+    MovieDataService.get(movieId)
+      .then((response) => setMovie(response.data))
+      .catch((e) => console.log(e))
+  }
+
+  useEffect(() => {
+    if (id) {
+      getMovie(id)
+    }
+  }, [id])
+
+  return <div></div>
 }
 export default Movie
