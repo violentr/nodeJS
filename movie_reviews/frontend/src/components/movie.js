d906c1789a10c674e958774f9d755af337b68606
Deniss B <den.b@itservicesgroup.co.uk>
2026-06-30 17:51:42 +0100

    Add simple style for movie component

--- movie_reviews/frontend/src/components/movie.js
+++ movie_reviews/frontend/src/components/movie.js
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from "react"
 import MovieDataService from "../services/movies"
 import { Link, useParams } from "react-router-dom"
+import Card from "react-bootstrap/Card"
+import Container from "react-bootstrap/Container"
+import Image from "react-bootstrap/Image"
+import Col from "react-bootstrap/Col"
+import Row from "react-bootstrap/Row"
 
-const Movie = () => {
+const Movie = (props) => {
   const { id } = useParams()
   const [movie, setMovie] = useState({
     id: null,
@@ -22,6 +27,29 @@ const Movie = () => {
     }
   }, [id])
 
-  return <div></div>
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col>
+            <Image src={movie.poster + "/100px250"} fluid />
+          </Col>
+          <Col>
+            <Card>
+              <Card.Header as="h5">{movie.title}</Card.Header>
+              <Card.Body>
+                <Card.Text>{movie.plot}</Card.Text>
+                {props.user && (
+                  <Link to={"/movies/" + id + "/review"}>Add Review</Link>
+                )}
+              </Card.Body>
+            </Card>
+            <br />
+            <h2>Reviews</h2>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  )
 }
 export default Movie
